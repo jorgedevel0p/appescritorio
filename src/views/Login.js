1ac6c04af459c520c8175fa4fb28304c12e4a78c
Chris <chrisdeveloper3001@gmail.com>
2022-09-21 23:50:33 -0300

    se agrega login con peticion http + loading

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from "react-router-dom"
+
+import { Navbar } from '../components/Navbar'
 
 export const Login = () => {
+  const navigate = useNavigate()
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleUsername = (evento) => {
+    const value = evento.target.value
+    setUsername(value)
+  }
+
+  const handlePassword = (evento) => {
+    const value = evento.target.value
+    setPassword(value)
+  }
+
+  const handleSubmit = (evento) => {
+    evento.preventDefault()
+
+    setIsLoading(true)
+
+    
+    /* if(!username || !password){
+      return
+    } */
+
+    const options = {
+      method: 'POST'
+    }
+
+    fetch('http://localhost:8000/api/token', options)
+      .then(res => res.json())
+      .then(respuesta => {
+        // Pasó todo OK
+
+        console.log('Aquí está la respuesta')
+        console.log(respuesta)
+
+        
+        navigate("/home")
+
+      })
+      .catch(error =>{
+        console.log('ha ocurrido un error, revisa que pasa en la consola')
+        console.log(error)
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+
+
+    
+  }
 
   return (
-
     <>
       <Navbar />
       <main class="py-5 px-4">
@@ -17,7 +73,7 @@ export const Login = () => {
                 <form>
                   <div class="form-group">
                     <label for="email">Email</label>
-                    <input name="email" id="email" class="form-control" type="email" />
+                    <input name="email" id="email" class="form-control" type="email" value={username} onChange={handleUsername} />
                   </div>
                   <div class="form-group">
                     <div class="row">
@@ -25,10 +81,27 @@ export const Login = () => {
                         <label for="password">Contraseña</label>
                       </div>
                     </div>
-                    <input name="password" id="password" class="form-control" type="password" />
+                    <input name="password" id="password" class="form-control" type="password" value={password} onChange={handlePassword} />
                   </div>
                   <div class="form-group mt-4 text-center">
-                    <button class="btn btn-primary btn-block">Ingresar</button>
+
+                    {/* {isLoading && (
+                      (<div class="spinner-border" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                      </div>)
+                    )} */}
+
+                    {
+                      isLoading 
+                      ? 
+                        (<div class="spinner-border" role="status">
+                          <span class="visually-hidden">Loading...</span>
+                        </div>)
+                      : (<button class="btn btn-primary btn-block" onClick={handleSubmit} /* disabled={!username || !password} */>Ingresar</button>)
+                    }
+
+
+                    
                   </div>
                 </form>
                 {/* <p class="text-center">O</p> */}
